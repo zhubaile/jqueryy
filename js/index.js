d6199df809789bf8090d1116cccce7c83263cd74
zhubaile <zyangzhuang@qq.com>
2018-03-22 22:31:00 +0800

    显示问题

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $(function(){
            }else{
                load(field.val());
                field.val("").focus();                            //清除输入框的值
-               yichu();
+               dianji();
            }
         }
     });
@@ -31,67 +31,62 @@ $(function(){
     });
     //删除按钮 点击事件    
     $("#todoItems").on("click", "a", function (event) {
-        var $target = $(event.target);
+        var $target = $(event.target);                    //target规定那个元素触发事件
         $target.closest("li").remove();
     });
     //checkbox 选择改变事件
     $("#todoItems").on("change", "input", function (event) {
         var $target = $(event.target),
             isChecked = $target.prop("checked"),
-            $targeLi = $target.closest("li");
+            $targeLi = $target.next("p");
             //可查看li已经添加此样式
         if (isChecked){
-            console.log("li添加样式error")
             $targeLi.addClass("error")
         }else{
-            console.log("li移除样式error")
             $targeLi.removeClass("error")
         }
     });
 
+
+
+
+    //根据复选框的未选中状态来显示span的数值
+    //缺陷：当选中复选框的时候，span里面的数值不会自动减少。
+    function dianji(){
+        var $aaa=$("li>input:checkbox").not("input:checked");
+        if(!$aaa){
+            $aaa.length-=1;
+        }
+        $("#lostcount").html($aaa.length);  //显示复选框未被选中的值
+    }
+
+
+
+
+
     //=================事件绑定 end====================
-
-
-    function load(val){                                  //用jq设置li的内容
+    function load(val) {                                  //用jq设置li的内容
         $(".all .fander,.all .center").show();
-                $(".lost").append("<li><input class='reo' type='checkbox' name='zbl'/> </input>" +
-                    "<p>"+val+"</p>" +
-                    "<a>×</a></li>");
+        $(".lost").append("<li><input class='reo' type='checkbox' name='zbl' /> </input>" +
+            "<p>" + val + "</p>" +
+            "<a>×</a></li>");
+    }
                     
         /*
         //以下删除，移动到上放，因为事件绑定放到这里，每次执行load方法就绑定一次事件
         //将会导致事件多次绑定，
-        $("#todoItems").on("mouseenter", "li", function (event) {
-         //10此 header-todo 的input回车后，此处当鼠标移入后执行十次。 
+         //10此 header-todo 的input回车后，此处当鼠标移入后执行十次。
          //这个地方要是还不懂在问我
+        $("#todoItems").on("mouseenter", "li", function (event) {
             var $target=$(event.target);
             $target.find("a").show()
-<<<<<<< HEAD
         });                                                           //这样是对的，mouseenter，mouseleave为hover的两个状态
         $("#todoItems") .on("mouseleave","li",function(event){
             var $target=$(event.target);
             $target.find("a").hide();
         });*/
-=======
-        });
-        $("#todoItems").on("mouseleave", "li", function (event) {
-            var $target = $(event.target);
-            $target.find("a").hide()
-        }); */
-        //$("li").hover(function(){$("a").show();},function(){$("a").hide();});
 
 
-
-
-        $("#lostcount").html(($("li>input").length));                    //li减少的时候，li.length的值不会跟着减少！
-                                                                    //设置的时候，选中状态的时候不会自动减少，我就清除代码了；不会写了
->>>>>>> 33b079dfb9fd1330e5f6174d7bcd8f11e5e993cd
-
-
-        $("#lostcount").html(($("li>input").length));                    //li减少的时候，li.length的值不会跟着减少！
-                                                                        // 设置的时候，选中状态的时候不会自动减少，我就清除代码了；不会写了
-
-    }
     //add by zhanglizhao 
     //解决li上面的叉，点击没反应问题，a元素的事件绑定未绑定上
     //解释：
@@ -100,53 +95,18 @@ $(function(){
     //$("#todoItems").on("click", "a", function (event) {});  
     //的意思是在id为todoItems的元素下的a元素上绑定事件，此时$("#todoItems")必须有值，
     //下面的a元素可以是后来添加的
-
-<<<<<<< HEAD
-    $("#todoItems").on("click", "a", function (event) {
-        var $target=$(event.target);                     //target规定那个元素触发事件
-        $target.closest("li").remove();
-       /* console.log("当前点击元素为：");
-        console.log($target)*/
-    });
-
-
-
-
-=======
->>>>>>> 33b079dfb9fd1330e5f6174d7bcd8f11e5e993cd
-
-   /* 
-   //如果想达到此效果，不能用这个方法，故移除此方法
-   function yichu(){                                  //次方法为选中复选框的时候添加error样式;
-        $('li>input').each(function(){           //选中的时候不知道为啥不增加class样式?
-            var $ele=$(this);
-            if ($ele.prop("checked")) {
-                $ele.next("p").addClass("error");
-            } else {
-                $ele.next("p").removeClass("error");
-            }
-        });
-    }
-<<<<<<< HEAD
-=======
-    yichu(); */
->>>>>>> 33b079dfb9fd1330e5f6174d7bcd8f11e5e993cd
-
-
-
-
-
-
     $(".center-all").click(function(){         //全选或者全不选按钮
         var i=0;
         var coll=$("li>input");
         if(coll[i].checked){
             for(var i=0;i<coll.length;i++){
                 coll[i].checked=false;
+                $("li>input").next("p").removeClass("error");
             }
         }else{
             for(var i=0;i<coll.length;i++){
                 coll[i].checked=true;
+                $("li>input").next("p").addClass("error");
             }
         }
     });
@@ -161,7 +121,7 @@ $(function(){
                 $ele.closest("li").show();
             }
         });
-    });         //
+    });
 
     $(".btn.two").click(function(){     //点击未完成按钮的时候显示所有checked=flase的li
         $(".btn.two").addClass("bode").siblings().removeClass("bode");
